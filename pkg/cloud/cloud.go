7971a462927bdebbcd32217804bb523464f81559
takonomura <takonomura@users.noreply.github.com>
2026-09-01 15:42:06 +0900

    Keep cluster ownership tag stable

--- pkg/cloud/cloud.go
+++ pkg/cloud/cloud.go
@@ -422,7 +422,7 @@ var _ Cloud = &cloud{}
 // initVariables initializes variables that depend on driver name.
 // Separated into a separate function from NewCloud so it can be called in tests.
 func initVariables() {
-	AwsEbsDriverTagKey = util.GetDriverName() + "/cluster"
+	AwsEbsDriverTagKey = "ebs.csi.aws.com/cluster"
 	AllowAutoIOPSIncreaseOnModifyKey = util.GetDriverName() + "/AllowAutoIOPSIncreaseOnModify"
 	IOPSPerGBKey = util.GetDriverName() + "/IOPSPerGb"
 }
